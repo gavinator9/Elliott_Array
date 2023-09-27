dd5ff0d21edacc41a66accb7bede4a4e66145b51
gavinator9 <epikdude@rocketmail.com>
2023-09-26 18:51:08 -0700

    Add version 1 project

---
+++ Elliott_arraysolution/Elliott_array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Elliott_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r= new Random(Environment.TickCount);
+            int ascii;
+            char input;
+
+            char[] container=new char[50];
+            Stopwatch sw= new Stopwatch();
+
+            var limits=new HashSet<int>() { 91, 92, 93, 94, 95, 96 };
+            sw.Start();
+            for (int i=0; i<50; i++ )
+            {
+                //    ascii = r.Next(65, 122);
+
+
+                ascii = r.Next(65, 123);
+
+                while (limits.Contains(ascii)==true)
+                {
+                    ascii = r.Next(65, 123);
+                }
+
+                input = (char)ascii;
+                container[i] = input;
+                limits.Add(ascii);
+
+            }
+            sw.Stop();
+            string output = new string(container);
+            Console.WriteLine(output);
+            Console.WriteLine("Elapsed time during generation: "+sw.Elapsed);
+        }
+    }
+}
